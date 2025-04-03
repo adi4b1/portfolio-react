16d6c43a94f33bd08891641459e05a46d1d1ecf8
adi4b1 <58321312+adi4b1@users.noreply.github.com>
2025-04-03 11:30:01 +0000

    4th apr code

--- src/pro/Info.jsx
+++ src/pro/Info.jsx
@@ -1,51 +1,51 @@
-import React, { useEffect, useRef } from 'react'
-import image from '../images/adi.jpeg'
+import React, { useEffect, useRef } from "react";
+import image from "../images/adi.jpeg";
 // import OpenModal from '../OpenModal'
 // import Button from 'react-bootstrap/Button';
 // import Modal from 'react-bootstrap/Modal';
 
 const Info = () => {
-  const getText=useRef(null)
-  window.addEventListener('scroll',function(e){
+  const getText = useRef(null);
+  window.addEventListener("scroll", function (e) {
     // console.log('scrollY',window.scrollY)
     // console.log('innerHeight',window.innerHeight)
     // console.log('totaldocu',document.documentElement.scrollHeight)
-    if(window.innerHeight){
-      console.log(e)
+    if (window.innerHeight) {
+      console.log(e);
     }
-  })
-  
-  useEffect(()=>{
-    let count=0;
-   
-    if(getText.current){
-      let outtext=`
+  });
+
+  useEffect(() => {
+    let count = 0;
+
+    if (getText.current) {
+      let outtext = `
       Hi, I am Adikesavulu Mitnala. I work as a Software Engineer.
-      `
+      `;
       // let data=getText.current.textContent
       // getText.current.style.display="none"
-      const animFunc=()=>{
-        let res=outtext.slice(0,count)
-        getText.current.textContent=res
-        count++;
-      }
-      // console.log(getText.current.textContent);
-      setInterval(animFunc,100)
-    }
-    
-  },[])
-  return (
-    <div className='infoBody'>
-        <div className='forText'>
-            <h1 ref={getText}></h1>
-        </div>
-        <div>
-            {/* <img src='/src/images/adi.jpeg' alt="adipic" /> */}
-            <img src={image} alt="pic" className='forImage' />
-        </div>
-        {/* <OpenModal/> */}
-    </div>
-  )
-}
+      const animFunc = () => {
+        let res = outtext.slice(0, count);
+        getText.current.textContent = res;
 
-export default Info
+        count++;
+      };
+      // console.log(getText.current.textContent);
+      setInterval(animFunc, 100);
+    }
+  }, []);
+  return (
+    <div className="infoBody">
+      <div className="forText">
+        <h1 ref={getText} className="forH1"></h1>
+      </div>
+      <div>
+        {/* <img src='/src/images/adi.jpeg' alt="adipic" /> */}
+        <img src={image} alt="pic" className="forImage" />
+      </div>
+      {/* <OpenModal/> */}
+    </div>
+  );
+};
+
+export default Info;
