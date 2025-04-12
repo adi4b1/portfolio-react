f548ba0991f6e68a518d0294055c5b2c02df3cb8
adi4b1 <58321312+adi4b1@users.noreply.github.com>
2025-04-12 17:51:46 +0000

    changeIn

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["rdrymr-5173.csb.app"],
+    allowedHosts: "rdrymr-4173.csb.app",
   },
 });
